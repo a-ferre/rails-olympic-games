7f029b3ee2c782a3437fc829f1e1d65405581b95
Albert Ferre <libraescorpio1982@gmail.com>
2021-03-12 09:15:25 +0900

    Added some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Sport.create(category: "Archery")
+Sport.create(category: "Artistic Gymnastics")
+Sport.create(category: "Badminton")
